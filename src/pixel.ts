a4af5a9a299dc6b7b5a9baa1cd7baa38a37788df
alon-l <2005alonlivne@gmail.com>
2021-12-25 20:01:20 +0200

    added docs to pixel

--- src/pixel.ts
+++ src/pixel.ts
@@ -1,6 +1,15 @@
+/**
+ * Represents a single pixel on the input canvas. A pixel is made of 3 channels: red, green and blue - each represented by an 8-bit unsigned integer.
+ */
 export type Pixel = [number, number, number];
 
+/**
+ * Utility functions for the {@link Pixel} tuple.
+ */
 export class PixelUtils {
+  /**
+   * Returns whether a pixel is included in a list of pixels.
+   */
   public static any(pixel: Pixel, matches: Pixel[] | Record<string, Pixel>): boolean {
     const values = Array.isArray(matches)
       ? matches
@@ -9,6 +18,9 @@ export class PixelUtils {
     return values.some(match => this.compare(pixel, match));
   }
 
+  /**
+   * Returns whether two pixels have the same values for all channels.
+   */
   public static compare(pixel1: Pixel, pixel2: Pixel): boolean {
     return pixel1.every((c, i) => c == pixel2[i]);
   }
