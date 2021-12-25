3228cb109df32131b1b8c10f043ec0b6c851e1f8
alon-l <2005alonlivne@gmail.com>
2021-12-25 22:29:50 +0200

    fixedtype verify size of pixels list

--- src/types/Type.ts
+++ src/types/Type.ts
@@ -12,6 +12,14 @@ export abstract class FixedType<T> extends Type<T> {
 
     this.size = size;
   }
+
+  public verify(): boolean {
+    if (Array.isArray(this.value) && this.value.length != this.size) {
+      throw new TypeError(`Number of pixels provided (${this.value.length}) mismatched expected ${this.size}`);
+    }
+
+    return false;
+  }
 }
 
 export abstract class DynamicType<T> extends Type<T> {
