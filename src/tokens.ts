fe367f452c35ec68a1737392e35c38555873b0df
alon-l <2005alonlivne@gmail.com>
2021-12-25 20:01:15 +0200

    added docs to tokens

--- src/tokens.ts
+++ src/tokens.ts
@@ -1,16 +1,25 @@
 import { Pixel } from './pixel';
 
+/**
+ * Represents the type object in the final {@link tokens} object.
+ */
 type TypeToken<V extends string> = {
   values: Record<V, Pixel>;
   id: Pixel
 }
 
+/**
+ * The structure of the {@link tokens} object.
+ */
 export type Tokens = {
   types: {
     boolean: TypeToken<'BOOL_TRUE' | 'BOOL_FALSE'>
   }
 }
 
+/**
+ * Represents the pixel colors associated to every token in the language.
+ */
 export const tokens: Tokens = {
   types: {
     boolean: {
